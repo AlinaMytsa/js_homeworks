4e79e4354e2776415d2798fccee8c373121a8b30
AlinaMytsa <act.mitsa@gmail.com>
2024-08-07 12:24:42 +0300

    hw 2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//Task #1
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+//Task#2
+const planet = 'Earth';
+const username = 'username';
+
+//Task#3
+const sum = '10' + 10;
+console.log(typeof sum);
