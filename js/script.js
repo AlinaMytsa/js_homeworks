2760b1664e35bdea0ec9f3384ba2b464a8f5fe26
AlinaMytsa <act.mitsa@gmail.com>
2024-08-07 12:36:27 +0300

    hw 3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const firstOperand = Number(prompt('Enter the first operand.'));
+const secondOperand = Number(prompt('Enter the second operand.'));
+
+console.log(`Приклад для складання: "Результат: ${firstOperand} + ${secondOperand} = ${firstOperand+secondOperand}"`);
+console.log(`Приклад для віднімання: "Результат: ${firstOperand} - ${secondOperand} = ${firstOperand-secondOperand}"`);
+console.log(`Приклад для множення: "Результат: ${firstOperand} * ${secondOperand} = ${firstOperand*secondOperand}"`);
+console.log(`Приклад для ділення: "Результат: ${firstOperand} / ${secondOperand} = ${firstOperand/secondOperand}"`);
