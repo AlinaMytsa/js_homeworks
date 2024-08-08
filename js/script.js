e1dc15d26d9232c2b5617d891733f000a0a183ba
AlinaMytsa <act.mitsa@gmail.com>
2024-08-08 18:49:45 +0300

    hw 3 updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 'use strict';
 
-const firstOperand = Number(prompt('Enter the first operand.'));
-const secondOperand = Number(prompt('Enter the second operand.'));
+const firstOperand = prompt('Enter the first operand.');
+const secondOperand = prompt('Enter the second operand.');
 
-console.log(`Приклад для складання: "Результат: ${firstOperand} + ${secondOperand} = ${firstOperand+secondOperand}"`);
-console.log(`Приклад для віднімання: "Результат: ${firstOperand} - ${secondOperand} = ${firstOperand-secondOperand}"`);
-console.log(`Приклад для множення: "Результат: ${firstOperand} * ${secondOperand} = ${firstOperand*secondOperand}"`);
-console.log(`Приклад для ділення: "Результат: ${firstOperand} / ${secondOperand} = ${firstOperand/secondOperand}"`);
+if (isNaN(firstOperand) || isNaN(secondOperand) || firstOperand === null || secondOperand === null) {
+    alert('Error: You have entered a non-numeric value or skipped the input.');
+} else {
+    console.log(`Приклад для складання: "Результат: ${firstOperand} + ${secondOperand} = ${firstOperand+secondOperand}"`);
+    console.log(`Приклад для віднімання: "Результат: ${firstOperand} - ${secondOperand} = ${firstOperand-secondOperand}"`);
+    console.log(`Приклад для множення: "Результат: ${firstOperand} * ${secondOperand} = ${firstOperand*secondOperand}"`);
+    console.log(`Приклад для ділення: "Результат: ${firstOperand} / ${secondOperand} = ${firstOperand/secondOperand}"`);
+}
+
