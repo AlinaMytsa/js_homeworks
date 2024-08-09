4bf74d91797aaf01a3f1cb264ad58f62c6a6c96f
AlinaMytsa <act.mitsa@gmail.com>
2024-08-09 17:54:34 +0300

    hw 6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+//1
+const name = prompt('Введіть своє імʼя.');
+const age = prompt('Введіть свій вік.');
+const likesProgramming = confirm('Чи подобається вам програмувати?');
+//2
+console.log(name, age, likesProgramming);
+console.log(`name: ${typeof name} type`);
+console.log(`age: ${typeof age} type`);
+console.log(`likesProgramming: ${typeof likesProgramming} type`);
+//3
+const greeting = `Привіт, ${name}!`;
+alert(greeting);
+//4
+const numericAge = Number(age);
+const nextAge = numericAge + 1;
+console.log(nextAge);
+//5
+const isAdult = numericAge >= 18;
+console.log(isAdult);
+
+
