4016d6d9aa64fea037315011b411506b2c1fe4eb
AlinaMytsa <act.mitsa@gmail.com>
2024-08-12 17:46:02 +0300

    hw 7 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const hours = prompt('Введіть кількість годин');
+
+if (isNaN(hours) || hours === null) {
+    alert('Введіть кільість годин.');
+} else {
+    const seconds = Number(hours) * 3600;
+    alert(`Ця кількість годин: ${hours} має таку кількість секунд: ${seconds}`);
+}
