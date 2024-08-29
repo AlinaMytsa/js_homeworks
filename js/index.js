cc2c008b1b9eeb35d12fb111c9c65772cf3ea493
AlinaMytsa <act.mitsa@gmail.com>
2024-08-29 17:29:41 +0300

    hw 16 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const padString = function (string, lenght, symbol, bool = true) {
+  const padStr = bool === false ? string.padStart(lenght, symbol) : string.padEnd(lenght, symbol);
+  if (string === undefined) {
+    return 'не введено строку';
+  } if (lenght === undefined) {
+    return 'не введено довжину';
+  } if (symbol === undefined) {
+    return 'не введено символ';
+  }
+
+  if (lenght < string.length) {
+    const newStr = string.substring(0, lenght);
+    return newStr;
+  }
+
+  return padStr;
+};
+const result = padString('hello', 42, '*');
+console.log(result);
