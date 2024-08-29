fecfb037ce41b2ac9eda8f3152effb1ee16ad9fc
AlinaMytsa <act.mitsa@gmail.com>
2024-08-29 17:42:09 +0300

    hw 17 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const removeElement = function (array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
+};
+const result = removeElement(arr, 5);
+console.log(result);
