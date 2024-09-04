74aa264d4f442ef8c343e602fd0293497f4b2dd4
AlinaMytsa <act.mitsa@gmail.com>
2024-09-04 22:04:28 +0300

    hw 21 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [];
+const fillTheArray = (array) => {
+  const userArrayLength = prompt('Введіть довжину масиву');
+
+  while (array.length < userArrayLength) {
+    const userArrayElement = +prompt('Введіть число-елемент масиву');
+    array.push(userArrayElement);
+  }
+  console.log(array);
+  array.sort((a, b) => a - b);
+  console.log(array);
+  const updatedArray = array.slice(2, 5);
+  console.log(updatedArray);
+};
+
+console.log(fillTheArray(arr));
