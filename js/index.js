7420cdac3b77d9aa893b8529e752297cb87f2bba
AlinaMytsa <act.mitsa@gmail.com>
2024-09-20 17:56:28 +0300

    hw 28 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// 1
+const getFactorial = (num) => {
+  if (num === 1) return num;
+  return num * getFactorial(num - 1);
+};
+console.log(getFactorial(3));
+
+// 2
+const pow = (num, degree) => {
+
+};
+console.log(pow(3, 2));
