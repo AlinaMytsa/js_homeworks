ce5fcd4447dd6e4e6c97136292925d1b56ecb290
AlinaMytsa <act.mitsa@gmail.com>
2024-09-20 17:15:44 +0300

    hw 26 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const insertWordInto = (initialStr = '') => {
+  let pseudoRandomIndex = 0;
+  return function (string) {
+    const arrayFromString = initialStr.split(' ');
+    arrayFromString.splice(pseudoRandomIndex, 0, string);
+    pseudoRandomIndex += 1;
+    return arrayFromString.join(' ');
+  };
+};
+
+const insert = insertWordInto('hello world');
+const result = insert('odesa');
+console.log(result);
+const result2 = insert('odesa');
+console.log(result2);
+const result3 = insert('odesa');
+console.log(result3);
