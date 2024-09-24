5f38f7d375265df44a2b277230c97e89683ebe8b
AlinaMytsa <act.mitsa@gmail.com>
2024-09-24 16:34:33 +0300

    hw 27 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const user = {
+  firstName: 'Alina',
+  lastName: 'Mytsa',
+};
+
+const getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+const apply = function (func, context, ...args) {
+  let result = null;
+  context.func = func;
+  result = context.func(...args);
+  delete context.func;
+
+  return result;
+};
+
+const bind = function (func, context, args = []) {
+  if (!func) return undefined;
+
+  return function () {
+    return apply(func, context, ...args);
+  };
+};
+
+const bindedGetFullName = bind(getFullName, user);
+console.log(bindedGetFullName());
