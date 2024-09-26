618c22a88861622844037e461dcd540feb61bfae
AlinaMytsa <act.mitsa@gmail.com>
2024-09-26 19:07:03 +0300

    hw 30 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const sum = () => {
+  let result = 0;
+  return function (num) {
+    result += num;
+    return result;
+  };
+};
+const sum1 = sum();
+console.log(sum1(3));
+console.log(sum1(5));
+console.log(sum1(20));
