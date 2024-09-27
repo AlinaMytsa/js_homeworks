1c0d3206ef4be56b589c1baf5110a78191a1011e
AlinaMytsa <act.mitsa@gmail.com>
2024-09-27 16:18:26 +0300

    hw 32 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const docBody = document.body;
+const table = () => {
+  for (let i = 1; i <= 100; i++) {
+    const row = document.createElement('tr');
+    const desc = document.createElement('td');
+    row.append(desc);
+    const description = document.createTextNode(`${i}`);
+    desc.append(description);
+    docBody.append(row);
+  }
+};
+table();
