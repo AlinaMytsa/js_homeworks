9655c99c8a8e03fe2717e9f60d57c110f9589fbf
AlinaMytsa <act.mitsa@gmail.com>
2024-09-27 15:36:33 +0300

    hw 31 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    return this.step;
+  },
+};
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+console.log(ladder.up().up().down().showStep());
