0182b84936cd1caf3b19665783d9873935900fa0
AlinaMytsa <act.mitsa@gmail.com>
2024-10-04 17:40:52 +0300

    hw 33 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const IMG_FOLDER = './images/';
+const IMG_EXT = '.jpg';
+const appendHTMLElement = (parentElement = null, elementToAppend = null) => {
+  if (!parentElement || !elementToAppend) return;
+  parentElement.append(elementToAppend);
+};
+
+const getRandomNumber = () => {
+  const num = Math.floor(Math.random() * 10);
+  if (num === 0) return 1;
+  return num;
+};
+
+const getRamdomImg = (imgName) => {
+  const img = document.createElement('img');
+  img.src = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+  img.alt = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+  img.title = `${IMG_FOLDER}${imgName + IMG_EXT}`;
+  return img;
+};
+const imageToAppend = getRamdomImg(getRandomNumber());
+appendHTMLElement(document.body, imageToAppend);
