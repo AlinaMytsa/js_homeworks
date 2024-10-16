ad4913573a038b811b6cb22aa99bb6e0bc3e0acf
AlinaMytsa <act.mitsa@gmail.com>
2024-10-16 18:51:19 +0300

    homework 37 completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+(function () {
+  const form = document.querySelector('[data-todo-form]');
+  const data = [];
+
+  const todoItemsContainer = document.querySelector('#todoItems');
+
+  const createTodoItem = ({ title, description }) => {
+    const wrapper = document.createElement('div');
+    wrapper.className = 'col-4';
+    wrapper.innerHTML = `
+              <div class="taskWrapper">
+                  <div class="taskHeading">${title}</div>
+                  <div class="taskDescription">${description}</div>
+              </div>
+    `;
+
+    return wrapper;
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const lastItem = data.at(-1);
+    const todoItemElement = createTodoItem(lastItem);
+    todoItemsContainer.prepend(todoItemElement);
+
+    event.target.reset();
+  };
+
+  const inputHandler = () => {
+    const inputs = document.querySelectorAll('input, textarea');
+    const dataOfInputs = Array.from(inputs).reduce((acc, { name, value }) => {
+      acc[name] = value;
+      return acc;
+    }, {});
+    data.push(dataOfInputs);
+    console.log(data);
+  };
+
+  form.addEventListener('submit', submitHandler);
+  form.addEventListener('change', inputHandler);
+}());
